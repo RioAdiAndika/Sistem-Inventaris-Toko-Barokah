4b09d65b9ecc22455975c4119f35f2ea8856e2dc
RioAdiAndika <rioadi4ndika@gmail.com>
2026-01-20 18:09:29 +0700

    perbaikan

--- database/migrations/2026_01_14_142245_create_satuans_table.php
+++ database/migrations/2026_01_14_142245_create_satuans_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,9 +14,19 @@ return new class extends Migration
     {
         Schema::create('satuans', function (Blueprint $table) {
             $table->id();
-            $table->string('nama'); // dus, bal, slop, pcs, dll
+            $table->string('nama')->unique(); // dus, bal, slop, pcs, dll
             $table->timestamps();
         });
+
+        // 🔥 INSERT DATA DEFAULT SATUAN
+        DB::table('satuans')->insert([
+            ['nama' => 'Bal',   ],
+            ['nama' => 'Dus',   ],
+            ['nama' => 'Karung',],
+            ['nama' => 'Pack',  ],
+            ['nama' => 'Pcs',   ],
+            ['nama' => 'Slop',  ],
+        ]);
     }
 
     /**
